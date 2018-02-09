150a8f13133a6c90be11b3cc5b0ce6479e883e17
Venkata Pratapaneni <vpratapaneni@Renkim.com>
2018-02-09 14:27:01 -0600

    first commit

---
+++ src/Table.js
@@ -0,0 +1,105 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from "material-ui/Table";
+import EditIcon from "material-ui/svg-icons/image/edit";
+import TrashIcon from "material-ui/svg-icons/action/delete";
+import CheckIcon from "material-ui/svg-icons/navigation/check";
+import TextField from "material-ui/TextField";
+import SelectField from 'material-ui/SelectField';
+
+const row = (
+  x,
+  i,
+  header,
+  handleRemove,
+  startEditing,
+  editIdx,
+  handleChange,
+  stopEditing
+) => {
+  const currentlyEditing = editIdx === i;
+  return (
+    <TableRow key={`tr-${i}`} selectable={false}>
+      {header.map((y, k) => (
+{y.prop!="item"(
+ <TableRowColumn key={`trc-${k}`}>
+          {currentlyEditing ? (
+            <TextField
+              name={y.prop}
+              onChange={e => handleChange(e, y.prop, i)}
+              value={x[y.prop]}
+            />
+          ) : (
+            x[y.prop]
+          )}
+        </TableRowColumn>
+):(
+
+<TableRowColumn key={`trc-${k}`}>
+          {currentlyEditing ? (
+           <SelectField
+        name={y.prop}
+        value={x[y.prop]}
+        onChange={e => handleChange(e, y.prop, i)}
+        maxHeight={200}
+          ) : (
+            x[y.prop]
+          )}
+        </TableRowColumn>
+)}
+))})
+      <TableRowColumn>
+        {currentlyEditing ? (
+          <CheckIcon onClick={() => stopEditing()} />
+        ) : (
+          <EditIcon onClick={() => startEditing(i)} />
+        )}
+      </TableRowColumn>
+      <TableRowColumn>
+        <TrashIcon onClick={() => handleRemove(i)} />
+      </TableRowColumn>
+    </TableRow>
+  );
+};
+
+export default ({
+  data,
+  header,
+  handleRemove,
+  startEditing,
+  editIdx,
+  handleChange,
+  stopEditing
+}) => (
+  <Table>
+    <TableHeader>
+      <TableRow>
+        {header.map((x, i) => (
+          <TableHeaderColumn key={`thc-${i}`}>{x.name}</TableHeaderColumn>
+        ))}
+        <TableHeaderColumn />
+        <TableHeaderColumn />
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {data.map((x, i) =>
+        row(
+          x,
+          i,
+          header,
+          handleRemove,
+          startEditing,
+          editIdx,
+          handleChange,
+          stopEditing
+        )
+      )}
+    </TableBody>
+  </Table>
+);
